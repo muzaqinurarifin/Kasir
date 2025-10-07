8f1948bb6c6db9674c72a9fa12b43d5d962bbb2f
DIMENEID <muzaqi.nurar4@gmail.com>
2025-10-07 12:28:13 +0700

    new

--- src/main/java/com/mycompany/kasir/Kasir.java
+++ src/main/java/com/mycompany/kasir/Kasir.java
@@ -11,6 +11,6 @@ package com.mycompany.kasir;
 public class Kasir {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!!!!!!!!!");
+        System.out.println("Hello World!");
     }
 }
